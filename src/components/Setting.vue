6f2e0a0cb59542dbba6c562dbb25b1b880671883
Alvin <AlvinWang627@gmail.com>
2022-05-14 22:23:59 +0800

    feat: edit word

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, inject, watch } from 'vue';
+import { ref, inject } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
 const gotItWords = inject('gotItWords');
 const unfamiliarWords = inject('unfamiliarWords');
@@ -7,6 +7,7 @@ const doNotKnowWords = inject('doNotKnowWords');
 const inputEnglish = ref('');
 const inputChinese = ref('');
 const wordsList = ref([]);
+const isEditing = ref(false);
 
 function importWords() {
   wordsList.value.push(
@@ -14,12 +15,19 @@ function importWords() {
     ...gotItWords.value,
     ...unfamiliarWords.value
   );
+  wordsList.value = wordsList.value.map((word) => ({
+    ...word,
+    isEditing: false,
+  }));
 }
 importWords();
 
 function addWord() {
-  if (inputChinese.value === '' || inputEnglish === '') {
-    return;
+  if (
+    inputChinese.value.trim().length === 0 ||
+    inputEnglish.value.trim().length === 0
+  ) {
+    return alert('不可以空白');
   }
   if (
     wordsList.value.some((wordEn) => wordEn.en === inputEnglish.value.trim())
@@ -35,10 +43,12 @@ function addWord() {
     id: uuidv4(),
     en: inputEnglish.value.trim(),
     ch: inputChinese.value.trim(),
+    isEditing: false,
   });
   inputChinese.value = '';
   inputEnglish.value = '';
 }
+
 function removeWord(_word) {
   wordsList.value = wordsList.value.filter((word) => word.id !== _word.id);
   doNotKnowWords.value = doNotKnowWords.value.filter(
@@ -48,6 +58,37 @@ function removeWord(_word) {
   unfamiliarWords.value = unfamiliarWords.value.filter(
     (word) => word.id !== _word.id
   );
+}
+// edit
+function toggleIsEditing(wordId) {
+  wordsList.value = wordsList.value.map((word) => {
+    if (word.id === wordId) {
+      return {
+        ...word,
+        isEditing: !word.isEditing,
+        EnCaches: word.en,
+        ChCaches: word.ch,
+      };
+    }
+    return word;
+  });
+}
+function updateWord({ wordId, EnWord, ChWord }) {
+  toggleIsEditing(wordId);
+}
+
+function cancelEdit(wordId) {
+  wordsList.value = wordsList.value.map((word) => {
+    if (word.id === wordId) {
+      return {
+        ...word,
+        en: word.EnCaches,
+        ch: word.ChCaches,
+      };
+    }
+    return word;
+  });
+  this.toggleIsEditing(wordId);
 }
 </script>
 
@@ -87,21 +128,74 @@ function removeWord(_word) {
       </thead>
       <tbody v-for="word in wordsList">
         <tr class="fs-2">
-          <td>{{ word.en }}</td>
-          <td>{{ word.ch }}</td>
-          <td class="delete" @click="removeWord(word)"></td>
+          <td>
+            <div v-show="!word.isEditing" @dblclick="toggleIsEditing(word.id)">
+              {{ word.en }}
+            </div>
+            <input
+              class="w-100 h-100 ps-2"
+              type="text"
+              v-show="word.isEditing"
+              v-model="word.en"
+            />
+          </td>
+          <td class="wordEn">
+            <div v-show="!word.isEditing" @dblclick="toggleIsEditing(word.id)">
+              {{ word.ch }}
+            </div>
+            <input
+              class="w-100 h-100 ps-2"
+              type="text"
+              v-show="word.isEditing"
+              v-model="word.ch"
+            />
+            <span class="cancel" v-show="word.isEditing"
+              ><i class="bi bi-x" @click.stop.prevent="cancelEdit(word.id)"></i
+            ></span>
+          </td>
+          <td class="icon">
+            <i
+              class="bi bi-check-lg"
+              v-show="word.isEditing"
+              @click.stop.prevent="
+                updateWord({
+                  wordId: word.id,
+                  EnWord: word.en,
+                  ChWord: word.ch,
+                })
+              "
+            ></i>
+            <i
+              class="bi bi-trash"
+              v-show="!word.isEditing"
+              @click="removeWord(word)"
+            ></i>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
 <style scoped>
-.delete:after {
-  content: 'x';
-  display: block;
-  line-height: 1.2;
-}
 td {
   word-break: break-all;
+}
+.icon {
+  cursor: pointer;
+}
+.icon :hover {
+  color: gray;
+}
+.wordEn {
+  position: relative;
+}
+.cancel {
+  position: absolute;
+  color: black;
+  right: 15px;
+  cursor: pointer;
+}
+.cancel:hover {
+  color: gray;
 }
 </style>
